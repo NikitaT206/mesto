3e11d0a3d91ec46f2803a264e2b84c32f5d0e13e
NikitaT206 <acid3602@yandex.ru>
2021-06-11 17:11:51 +1100

    feat: написать js код

---
+++ index.js
@@ -0,0 +1,39 @@
+let profileEditButton = document.querySelector('.profile__edit-button')
+let popup = document.querySelector('.popup')
+let popupCloseButton = document.querySelector('.popup__close-button')
+let inputJob = document.querySelector('.input-job')
+let inputName = document.querySelector('.input-name')
+let profileName = document.querySelector('.profile__name')
+let profileJob = document.querySelector('.profile__job')
+let popupForm = document.querySelector('.popup__form')
+
+inputName.value = profileName.textContent
+inputJob.value = profileJob.textContent
+
+
+function formSubmitHandler(evt) {
+  evt.preventDefault()
+
+  if (!inputName.value || !inputJob.value) {
+    console.log('Оба поля должны быть заполнены')
+  }
+  else {
+    profileName.textContent = inputName.value
+    profileJob.textContent = inputJob.value
+    popup.classList.remove('popup_opened')
+  }
+}
+
+popupForm.addEventListener('submit', formSubmitHandler)
+
+profileEditButton.addEventListener('click', function (evt) {
+  evt.preventDefault()
+  popup.classList.add('popup_opened')
+})
+
+popupCloseButton.addEventListener('click', function (evt) {
+  evt.preventDefault()
+  inputName.value = profileName.textContent
+  inputJob.value = profileJob.textContent
+  popup.classList.remove('popup_opened')
+})
